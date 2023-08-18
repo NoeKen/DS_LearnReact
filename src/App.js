b4bb4624fbafcf5a0a3a59167860a25444add856
NoeKen <kenfaclnoe@gmail.com>
2023-08-18 23:53:52 +0100

    resolution d'une erreur

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 // import { Routes } from "./routes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Views/Home";
+import Home from "./Views/home/Home";
 import About from "./Views/about/About";
 import { NavBar } from "./components/navbar";
 
